9bcd1c178c167d5eb0cf6f9f5d4f7c53191ad6a9
cww97_mac <cww970329@qq.com>
2017-06-04 17:00:32 +0800

    codes about docs to emotions cluster

---
+++ emotion/merge.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+import overlap
+import apriori
+
+
+def sim_clu(clu_a, clu_b):
+    if type(clu_a) == int or type(clu_b) == int: return 0
+    k = (len(clu_a) * len(clu_b)) / 2
+    if k == 0: k = len(clu_a) * len(clu_b)
+    sims = []
+    for a in clu_a:
+        for b in clu_b:
+            sims.append(overlap.sim(a,b))
+    sims.sort()
+    sum = 0.0
+    for i in range(k):
+        sum += sims[-i-1]
+    return 1. * sum / k
+
+
+# 获取一堆情感簇的相似度矩阵
+def get_mat(clusters):
+    mat = []
+    max_sim = row = col = 0
+
+    for i in range(len(clusters)):
+        mat.append([])
+        for j in range(len(clusters)):
+            mat[i].append(0)
+
+    for i in range(len(clusters)):
+        for j in range(len(clusters)):
+            if i == j:
+                mat[i][j] = 0
+            else:
+                mat[i][j] = mat[j][i] = sim_clu(clusters[i], clusters[j])
+                if mat[i][j] > max_sim:
+                    max_sim = mat[i][j]
+                    row = i
+                    col = j
+    return mat, max_sim, row, col
+
+
+#合并a, b两个情感簇
+#合并其特征词，讲原本属于b的doc丢进a中
+def merge(a, b):
+    for word in clusters[b]:
+        if not word in clusters[a]:
+            clusters[a].append(word)
+    for i in range(len(docs)):
+        if belong[i] == b:
+            belong[i] = a
+    clusters[b] = a
+
+
+# 阈值为k，将cluster合并到还剩n个
+def main(clusters, k, n):
+    while True:
+        mat, max_sim, row, col = get_mat(clusters)
+        if max_sim < k:
+            return clusters
+        else:
+            merge(row, col)
+
+
+if __name__ == '__main__':
+    docs = apriori.D
+    clusters = apriori.apriori(docs, 0.7)
+    belong  = overlap.main(docs,clusters)
+    result = main(clusters, 0.7, 6)
+    print result
---
+++ emotion/overlap.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+import apriori
+
+
+# 计算两个词语语义相似度
+# xb喵喵喵
+def sim(f, t):
+    return float(f==t)
+
+
+# 计算文档doc对聚类cluster的隶书度
+def score(cluster, doc):
+    ans = 0.0
+    for t in doc:
+        max_sim = 0.0
+        for f in cluster:
+            max_sim = sim(f,t)
+        ans += max_sim
+    return ans / len(doc)
+
+
+# 去重重复情感簇，
+# 实现方式为每个文档找到一个与之匹配度最大的情感度
+# 返回belong[文档index] = 情感簇index
+def main(docs, clusters):
+    belong = {}
+
+    for i in range(len(docs)):
+        max_sco = 0.0
+        bel = 0
+        for j in range(len(clusters)):
+            sco = score(clusters[j], docs[i])
+            if sco > max_sco:
+                max_sco = sco
+                bel = j
+        belong[i] = bel
+        #print(docs[i], ' ', clusters[j])
+
+    return belong
+
+if __name__ == '__main__':
+    docs = apriori.D
+    clusters = apriori.apriori(docs, 0.7)
+    main(docs, clusters)
